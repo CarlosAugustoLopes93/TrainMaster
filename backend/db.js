1e5816e2fea614e6a485be5e90db22fb910cef1b
CarlosAugustoLopes93 <carlosaugustolopes@rede.ulbra.br>
2024-11-19 20:34:04 -0300

    Alteração DB.JS

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,16 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+require('dotenv').config(); // Carrega as variáveis de ambiente
 
+// Usando as variáveis de ambiente para configurar a conexão com o banco de dados
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'trainmaster',
-  password: 'gutinho12',
-  port: 5432,
+  user: process.env.DB_USER,       // Variável de ambiente para o usuário
+  host: process.env.DB_HOST,       // Variável de ambiente para o host do banco de dados
+  database: process.env.DB_NAME,   // Variável de ambiente para o nome do banco de dados
+  password: process.env.DB_PASSWORD, // Variável de ambiente para a senha do banco
+  port: process.env.DB_PORT,       // Variável de ambiente para a porta
 });
 
+// Conectando ao banco de dados e tratando erros
 pool.connect()
   .then(() => console.log('Conectado ao banco de dados PostgreSQL'))
   .catch((err) => console.error('Erro ao conectar ao banco de dados', err));
