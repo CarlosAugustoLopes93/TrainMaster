f30ea00d4cb8ad163d90a420132c1502bcf74c0b
CarlosAugustoLopes93 <carlosaugustolopes@rede.ulbra.br>
2024-11-20 15:57:22 -0300

    Corrige caminho para servir arquivos estáticos no Render

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
-require('dotenv').config();  // Carrega as variáveis do arquivo .env
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path'); // Importa o 'path' para manipulação de caminhos
-const userRoutes = require('./routes/userRoutes');
-const trainerRoutes = require('./routes/trainerRoutes');
-const workoutRoutes = require('./routes/workoutRoutes');
-const exerciseRoutes = require('./routes/exerciseRoutes');
-const authRoutes = require('./routes/authRouter');
-const pool = require('./db'); // Importa o pool do arquivo db.js
-
+const path = require('path');
+const userRoutes = require('./routes/userRoutes'); // Rotas de usuário
+const trainerRoutes = require('./routes/trainerRoutes'); // Rotas de treinador
+const workoutRoutes = require('./routes/workoutRoutes'); // Rotas de treino
+const exerciseRoutes = require('./routes/exerciseRoutes'); // Rotas de exercícios
+const authRoutes = require('./routes/authRouter'); // Rotas de autenticação
+const pool = require('./db'); // Banco de dados
 const app = express();
+
+// Adicionando log para confirmar que o servidor iniciou
+console.log('Iniciando o servidor...');
 
 // Middleware
 app.use(cors());
@@ -29,17 +31,17 @@ app.use('/api/auth', authRoutes);
 // Serve os arquivos estáticos do React
 if (process.env.NODE_ENV === 'production') {
   // Serve a pasta 'build' para produção, ajustando o caminho para o diretório correto
-  app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));  // Subindo um nível para sair da pasta 'backend'
+  app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));  // Ajuste o caminho
 
   // Roteamento para todas as outras rotas, garantindo que o React seja servido corretamente
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));  // Ajuste no caminho
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));  // Ajuste o caminho
   });
 }
 
-// Tratamento de erros para capturar falhas inesperadas
+// Tratamento de erros
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error('Erro no servidor:', err);  // Adicionando log para erro no servidor
   res.status(500).send('Algo deu errado!');
 });
 
